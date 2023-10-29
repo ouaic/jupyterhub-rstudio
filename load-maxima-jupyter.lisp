4bc908c54c9b33ac8922f44d165372a63a01f175
ouaic <115682698+ouaic@users.noreply.github.com>
2023-10-29 23:49:25 +0900

    Add files via upload
    Copied from
    https://github.com/robert-dodier/maxima-jupyter/blob/master/load-maxima-jupyter.lisp

---
+++ load-maxima-jupyter.lisp
@@ -0,0 +1,17 @@
+(maxima::$load "mactex-utilities")
+(maxima::$load "stringproc")
+
+#+quicklisp
+  (if (asdf:find-system :maxima-jupyter nil)
+    (asdf:load-system :maxima-jupyter)
+    (progn
+      (pushnew (make-pathname :device (pathname-device *load-truename*)
+                              :directory (pathname-directory *load-truename*))
+               ql:*local-project-directories*)
+      (ql:register-local-projects)
+      (ql:quickload :maxima-jupyter)))
+
+#-quicklisp
+  (asdf:load-system :maxima-jupyter)
+
+; (declaim (optimize (speed 0) (space 0) (debug 3) (safety 3)))
